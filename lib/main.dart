685b6aca687bf1a41e61e09ead39d8d8a38bb4ef
çınar atakul <163647886+cinaratac@users.noreply.github.com>
2025-08-07 01:43:00 +0300

    anamenü silindi

--- lib/main.dart
+++ lib/main.dart
@@ -14,132 +14,60 @@ Future<void> main() async {
     DeviceOrientation.portraitUp,
     DeviceOrientation.portraitDown,
   ]);
-  runApp(const MyApp());
-}
-
-class MyApp extends StatefulWidget {
-  const MyApp({super.key});
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  static _MyAppState? instance;
-
-  bool startGame = false;
-
-  @override
-  void initState() {
-    super.initState();
-    instance = this;
-  }
-
-  static void returnToMainMenu() {
-    instance?.setState(() {
-      instance?.startGame = false;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    if (startGame) {
-      return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: GameWidget(
-          game: RotatingArrowGame(),
-          initialActiveOverlays: const ['mainMenuButton'],
-          overlayBuilderMap: {
-            'levelMenuButton': (context, game) {
-              return Positioned(
-                top: 10,
-                right: 10,
-                child: TextButton(
-                  style: TextButton.styleFrom(
-                    backgroundColor: Colors.grey,
-                    foregroundColor: Colors.white,
-                  ),
-                  onPressed: () {
-                    Navigator.of(context).pushReplacement(
-                      PageRouteBuilder(
-                        transitionDuration: Duration(milliseconds: 400),
-                        pageBuilder: (_, __, ___) => LevelSelectScreen(),
-                        transitionsBuilder: (_, animation, __, child) {
-                          return FadeTransition(
-                            opacity: animation,
-                            child: child,
-                          );
-                        },
-                      ),
-                    );
-                  },
-                  child: Text('Menü'),
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: GameWidget(
+        game: RotatingArrowGame(),
+        overlayBuilderMap: {
+          'levelMenuButton': (context, game) {
+            return Positioned(
+              top: 10,
+              right: 10,
+              child: TextButton(
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.grey,
+                  foregroundColor: Colors.white,
                 ),
-              );
-            },
-            'shopButton': (context, game) {
-              return Positioned(
-                top: 10,
-                left: 10,
-                child: TextButton(
-                  style: TextButton.styleFrom(
-                    backgroundColor: Colors.grey,
-                    foregroundColor: Colors.white,
-                  ),
-                  onPressed: () {
-                    Navigator.of(context).push(
-                      MaterialPageRoute(
-                        builder: (context) =>
-                            ShopScreen(game: game as RotatingArrowGame),
-                      ),
-                    );
-                  },
-                  child: Text('Shop'),
-                ),
-              );
-            },
-            'mainMenuButton': (context, game) {
-              return Positioned(
-                bottom: 10,
-                right: 10,
-                child: ElevatedButton(
-                  onPressed: () {
-                    _MyAppState.returnToMainMenu();
-                  },
-                  child: Text('Ana Menü'),
-                ),
-              );
-            },
+                onPressed: () {
+                  Navigator.of(context).pushReplacement(
+                    PageRouteBuilder(
+                      transitionDuration: Duration(milliseconds: 400),
+                      pageBuilder: (_, __, ___) => LevelSelectScreen(),
+                      transitionsBuilder: (_, animation, __, child) {
+                        return FadeTransition(opacity: animation, child: child);
+                      },
+                    ),
+                  );
+                },
+                child: Text('Menü'),
+              ),
+            );
           },
-        ),
-      );
-    } else {
-      return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: Scaffold(
-          backgroundColor: Colors.black,
-          body: Center(
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Text(
-                  'Ball Game',
-                  style: TextStyle(
-                    fontSize: 40,
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
-                  ),
+          // Shop button overlay positioned top left
+          'shopButton': (context, game) {
+            return Positioned(
+              top: 10,
+              left: 10,
+              child: TextButton(
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.grey,
+                  foregroundColor: Colors.white,
                 ),
-                SizedBox(height: 20),
-                ElevatedButton(
-                  onPressed: () => setState(() => startGame = true),
-                  child: Text('Başla'),
-                ),
-              ],
-            ),
-          ),
-        ),
-      );
-    }
-  }
+                onPressed: () {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (context) =>
+                          ShopScreen(game: game as RotatingArrowGame),
+                    ),
+                  );
+                },
+                child: Text('Shop'),
+              ),
+            );
+          },
+        },
+      ),
+    ),
+  );
 }
